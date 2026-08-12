eae38849c6b0f809c77e7bdd67946a31ae1f8877
Sierra <Parkersierra40@gmail.com>
2026-08-12 01:26:50 -0400

    Finish homepage styling

--- Website/js/script.js
+++ Website/js/script.js
@@ -1,14 +1,18 @@
 const whyUs = [
-    {image: "images/WhyUS-icon-1.png",
+    {
+        image: "images/WhyUS-icon-1.png",
         text: "Premium Beans"
     },
-    {image: "images/WhyUS-icon.png",
+    {
+        image: "images/WhyUS-icon.png",
         text: "Fresh Pastries"
     },
-    {image: "images/WhyUS-icon-2.png",
+    {
+        image: "images/WhyUS-icon-2.png",
         text: "Free Wifi"
     },
-    {image: "images/WhyUS-icon-3.png",
+    {
+        image: "images/WhyUS-icon-3.png",
         text: "Locally Roasted"
     },
 ];
@@ -25,4 +29,69 @@ for (let i = 0; i < whyUs.length * 3; i++) {
             <span>${item.text}</span>
         </div>
     `;
+}
+const testimonial = [
+    {
+        name: "Jessica M.",
+        message: " The coffee is the best ive ever tasted!",
+        rating: 5
+    },
+    {
+        name: "David L.",
+        message: "Cozy vibes, great service, and even better coffee",
+        rating: 5
+    },
+    {
+        name: "Sarah K.",
+        message: "I come here every week. Highly reccommend!",
+        rating: 5
+    }
+]
+const testimonialContainer = document.getElementById("testimonial-container")
+testimonialContainer.innerHTML = testimonial.map(testimonial => `
+    <div class="testimonial-card">
+        <p>"${testimonial.message}"</p>
+        <h3>${testimonial.name}</h3>
+        <span>★★★★★</span>
+    </div>
+`).join("");
+
+const viewMoreButton = document.querySelectorAll('[data-open-button]')
+const closeButton = document.querySelectorAll('[data-close-button]')
+const overlay = document.getElementById('overlay')
+
+viewMoreButton.forEach(button => {
+    button.addEventListener('click', () => {
+        const modal = document.querySelector(button.dataset.openButton)
+        openModel(modal)
+    })
+})
+
+overlay.addEventListener('click', () => {
+    const modals = document.querySelectorAll('.our-coffee.active')
+
+    modals.forEach(modal => {
+        closeModel(modal)
+    })
+})
+
+closeButton.forEach(button => {
+    button.addEventListener('click', () => {
+        const modal = button.closest('.our-coffee')
+        closeModel(modal)
+    })
+})
+
+function openModel(modal) {
+    if (modal == null) return
+
+    modal.classList.add('active')
+    overlay.classList.add('active')
+}
+
+function closeModel(modal) {
+    if (modal == null) return
+
+    modal.classList.remove('active')
+    overlay.classList.remove('active')
 }
